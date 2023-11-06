7fb17751b1c59b5b6444998ee7673855d7f966f4
emptyset <emptierset@gmail.com>
2023-11-06 12:03:43 -0500

    feat: basic Kt dual card support

--- sidcon/parse.py
+++ sidcon/parse.py
@@ -1,7 +1,9 @@
+import collections
 import csv
 import logging
 import pprint
 
+from sidcon.alien import *
 from sidcon.card import (
     Card,
     CreatedCard,
@@ -29,6 +31,19 @@ skipped_card_factions = [
 ]
 
 skipped_front_names = [
+    "Expansive Social",
+    "Diffusion",
+    "Hand Crafted",
+    "Polyutility Components",
+    "Anarchic Sacrificial",
+    "High-Risk Laboratories",
+    "Vast Distance",
+    "Bending Engines",
+    "Diverse",
+    "Interspecies Housing",
+    "Alien Cultural",
+    "Inspiration",
+    #
     "Incoming Donations",
     "Starting Race Card",
     "Phases",
@@ -111,10 +126,109 @@ def all_cards() -> list["Card | KtDualCard"]:
     return cards
 
 
-if __name__ == "__main__":
+def validate_tech_cards():
+    cards = [c for c in all_cards() if isinstance(c, TechnologyCard)]
+
+    fronts = [c.front for c in cards]
+    assert all(len(f.features) == 1 for f in fronts)
+    assert len(set([f.name for f in fronts])) == 21
+
+    fronts_by_name = collections.defaultdict(list)
+    for front in fronts:
+        fronts_by_name[front.name].append(front)
+
+    for front_name, like_fronts in fronts_by_name.items():
+        assert len(set([f.input_value for f in like_fronts])) == 1
+        upgrade_pairs = [
+            tuple(sorted(f.upgrades[0][0], key=lambda t: str(t))) for f in like_fronts
+        ]
+        assert len(set(upgrade_pairs)) == 1
+
+    backs = [c.back for c in cards]
+    assert all(len(f.features) == 1 for f in backs)
+
+    back_names = set([f.name for f in backs])
+    assert len(back_names) == 22
+    # Imdril has Galactic Reunion instead of Galactic Colonization........
+
+    backs_by_name = collections.defaultdict(list)
+    for back in backs:
+        backs_by_name[back.name].append(back)
+
+    # Some of the unity upgrades produce the same output value as others, but most do not.
+    unity_same_back_names = [
+        "Multispecies Hybrid Cultures",
+        "Full Interspecies Integration",
+        "Living Infrastructure",
+        "Galactic Domination",
+        "Stasis Field",
+        "Macroscale Teleportation",
+        "Dyson Swarms",
+        "Galactic Colonization",
+    ]
+
+    for back_name, like_backs in backs_by_name.items():
+        # Imdril has Galactic Reunion instead of Galactic Colonization........
+        if back_name == "Galactic Reunion":
+            back_name = "Galactic Colonization"
+        unique_output_values = set([f.output_value for f in like_backs])
+        if back_name in unity_same_back_names:
+            assert len(unique_output_values) == 1
+        else:
+            assert len(unique_output_values) == 2
+            output1, output2 = unique_output_values
+            assert abs(output1 - output2) == 0.5
+
+
+def pprint_species_cards(species):
     cards = all_cards()
-    #starting_cards = list(filter(lambda c: isinstance(c, Starting), cards))
-    #pprint.pprint(starting_cards)
-    #print(len(starting_cards))
-    pprint.pprint(cards)
-    print(len(cards))
+    for card in cards:
+        object.__setattr__(
+            card.front, "upgrades", [(us, uface.name) for us, uface in card.front.upgrades]
+        )
+        if card.back is not None:
+            object.__setattr__(
+                card.back, "upgrades", [(us, uface.name) for us, uface in card.back.upgrades]
+            )
+    cards = [card for card in cards if hasattr(card, "species") and card.species == species]
+
+    starting_cards = [c for c in cards if isinstance(c, StartingCard)]
+    tech_cards = sorted(
+        [c for c in cards if isinstance(c, TechnologyCard)], key=lambda c: c.era.value
+    )
+    other_cards = [
+        c for c in cards if not isinstance(c, TechnologyCard) and not isinstance(c, StartingCard)
+    ]
+
+    starting_faces = []
+    for card in starting_cards:
+        starting_faces.append(card.front)
+        starting_faces.append(card.back)
+
+    tech_faces = []
+    for card in tech_cards:
+        tech_faces.append(card.front)
+        tech_faces.append(card.back)
+
+    other_faces = []
+    for card in other_cards:
+        other_faces.append(card.front)
+        other_faces.append(card.back)
+
+    print("############## STARTING CARDS ##############")
+    pprint.pprint(starting_faces)
+    print()
+    print()
+    #print("############## TECH CARDS ##############")
+    #pprint.pprint(tech_faces)
+    #print()
+    #print()
+    print("############## OTHER CARDS ##############")
+    pprint.pprint(other_faces)
+    print()
+    print()
+
+
+if __name__ == "__main__":
+    validate_tech_cards()
+    pprint_species_cards(Kjasjavikalimm)
